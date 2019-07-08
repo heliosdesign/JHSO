a0e1ae91db9c383deeddedcbb9fdbb2131e792a9
Mike Mali <michaeltdmali@gmail.com>
2019-07-08 13:12:49 -0400

    Quiz features added
    Added quiz features to display correct/incorrect answer based on user choice.

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ window.onload = function() {
 
   // _ DOM REFERENCES *********************************************************
 
-  const slides = [...document.querySelectorAll('.slide')]
-  const btnNext = [...document.querySelectorAll('.btn-next')]
-  const btnPrev = [...document.querySelectorAll('.btn-prev')]
+  const slides = document.querySelectorAll('.slide')
+  const btnNext = document.querySelectorAll('.btn-next')
+  const btnPrev = document.querySelectorAll('.btn-prev')
   const video = document.querySelector('.video-media')
   const progressBar = document.querySelector('.video-progress-bar')
   const btnPlay = document.querySelector('.btn-video.mod-play')
   const btnRestart = document.querySelector('.btn-video.mod-restart')
   const btnSkip = document.querySelector('.btn-video.mod-skip')
+  const btnYes = document.querySelectorAll('.btn-quiz.mod-yes')
+  const btnNo = document.querySelectorAll('.btn-quiz.mod-no')
+  const btnQuestionReset = document.querySelectorAll('.btn-prev.mod-question-reset')
 
   // _ FUNCTIONS **************************************************************
 
@@ -84,12 +87,52 @@ window.onload = function() {
     progressBar.style.flexBasis = `${percent}%`
   }
 
+  function answerQuestion() {
+    const questionPanel = document.querySelector(`.slide-${index} .mod-question`)
+    const answerPanel = document.querySelector(`.slide-${index} .mod-answer`)
+    const answerHeading = document.querySelector(`.slide-${index} .mod-answer .type-heading`)
+
+    if (this.dataset.correct === 'true') {
+      answerHeading.textContent = 'Correct.'
+      answerPanel.classList.add('mod-correct')
+    } else {
+      answerHeading.textContent = 'Incorrect.'
+      answerPanel.classList.add('mod-incorrect')
+    }
+
+    TweenMax.to(questionPanel, 1, {
+      opacity: 0,
+      ease: Expo.easeInOut,
+      onComplete: function() {
+        questionPanel.style.display = 'none'
+      }
+    })
+  }
+
+  function resetQuestion() {
+    const question = document.querySelector(`.slide-${index - 1} .mod-question`)
+    question.style.opacity = 1
+    question.style.display = 'flex'
+  }
+
   btnNext.forEach(btn => {
     btn.addEventListener('click', nextSlide)
   })
 
   btnPrev.forEach(btn => {
     btn.addEventListener('click', prevSlide)
+  })
+
+  btnYes.forEach(btn => {
+    btn.addEventListener('click', answerQuestion)
+  })
+
+  btnNo.forEach(btn => {
+    btn.addEventListener('click', answerQuestion)
+  })
+
+  btnQuestionReset.forEach(btn => {
+    btn.addEventListener('click', resetQuestion)
   })
 
   // _ EVENT HANDLERS *********************************************************
